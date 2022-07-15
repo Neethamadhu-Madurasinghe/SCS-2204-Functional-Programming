4f8c1534b305ede5e43d81d3e97bb78bd8701634
Neethamadhu-Madurasinghe <87432896+Neethamadhu-Madurasinghe@users.noreply.github.com>
2022-07-15 22:37:44 +0530

    Submit tutorial 3 answers

---
+++ Tutorial 3/20001223_3.scala
@@ -0,0 +1,41 @@
+object Tutorial3 {
+  def main(args: Array[String]): Unit = {
+    // Question 1
+    printf("Area of disk : %.2f\n", areaOfDisk(5f));
+
+    // Question 2
+    printf("35 Celsius in Fahrenheit: %.2f\n", celsiusToFahrenheit(35f));
+
+    // Question 3
+    printf("Volume of sphere: %.2f\n", areaOfSphere(5f));
+
+    // Question 4
+    printf("Total cost for 60 books: %.2f\n", totalWholeSaleCost(60));
+
+    // Question 5
+    printf("Total running time: %.0f Minutes", timeEasyPace(2) + timeTempo(3) + timeEasyPace(2))
+  }
+}
+
+
+// Question 1
+def areaOfDisk(r: Float): Float = Math.PI.toFloat * r * r
+
+ // Question 2
+ def celsiusToFahrenheit(t: Float): Float = t * 1.8f + 32.0f;
+
+ // Question 3
+ def areaOfSphere(r: Float): Float = (4.0f/3.0f) * Math.PI.toFloat * r * r;
+
+ // Question 4
+ def totalWholeSaleCost(books: Int): Float = bookCost(books) + shippingCost(books);
+
+ def bookCost(books: Int): Float = books * 24.5f * (1 - 0.4f);
+ def shippingCost(books: Int): Float = books match {
+    case x if x <= 50 =>  books * 3.0f
+    case x => (50 * 3.0f) + ((books - 50) * .75f)    
+ }
+
+ // Question 5
+ def timeEasyPace(d: Float): Float = d * 8.0f;
+ def timeTempo(d: Float): Float = d * 7.0f;
