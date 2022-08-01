c3d667df66efcb254dc04d7a362f3e99d8872a22
Neethamadhu-Madurasinghe <87432896+Neethamadhu-Madurasinghe@users.noreply.github.com>
2022-08-01 20:01:42 +0530

    Tutorial 4 answers upload

---
+++ Tutorial 4/20001223_4_q3.scala
@@ -0,0 +1,36 @@
+object Tutorial4 {
+def main(args: Array[String]): Unit = {
+    // Question 3
+    println(formatNames("Benny", _toUpper)())
+    println(formatNames("Niroshan", _toUpper)(0::1::Nil))
+    println(formatNames("Saman", _toLower)())
+    println(formatNames("Kumara", _toUpper)(0::"Kumara".length-1::Nil))
+  }
+} 
+
+// Question 3
+def _toUpper(word: String): String = word.toUpperCase()
+def _toLower(word: String): String = word.toLowerCase()
+
+
+
+def formatNames(name: String, f: String => String)(format: List[Int] = List()): String = {
+  if(format.isEmpty)  {
+    f(name)
+  
+  }else {
+    var temp = ""
+    for(i <- 0 to name.length() - 1) {
+      if(format.contains(i)) {
+        temp = temp + f(name.charAt(i).toString())
+      }else {
+        temp = temp + name.charAt(i).toString()
+      }
+    }
+    temp
+  }
+}
+
+
+
+
