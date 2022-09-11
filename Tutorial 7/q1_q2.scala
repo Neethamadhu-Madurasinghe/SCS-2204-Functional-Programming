6c0a9b0225605e974211fdab27c509d97f4108bf
Neethamadhu-Madurasinghe <87432896+Neethamadhu-Madurasinghe@users.noreply.github.com>
2022-09-11 15:12:42 +0530

    Tutorial 7

---
+++ Tutorial 7/q1_q2.scala
@@ -0,0 +1,45 @@
+object Main {
+  def main(args: Array[String]) = {
+    val x = new Rational(3, 4);
+    val y = new Rational(5, 8);
+    val z = new Rational(2, 7);
+
+    println(x);
+    println(y);
+    println(z);
+
+    println(x-y-z);
+
+  } 
+}
+
+class Rational(x: Int, y: Int) {
+  require(y > 0, "Denominator Must be greater then 0")
+
+  private def GCD(a: Int, b: Int): Int = b match {
+    case 0 => a
+    case x if x > a => GCD(x, a)
+    case _ => GCD(b, a % b)
+  }
+
+  val numerator = x / GCD(Math.abs(x), y)
+  val denominator = y / GCD(Math.abs(x), y)
+
+  def neg(): Rational = {
+    new Rational(-1 * this.numerator, this.denominator)
+  }
+
+  def +(that: Rational): Rational = {
+    val n = (this.numerator * that.denominator) + (that.numerator * this.denominator)
+    val d = this.denominator * that.denominator 
+    new Rational(n, d)
+  }
+
+  def -(that: Rational): Rational = {
+    this + that.neg()
+  }
+
+  override def toString(): String = {
+    "(" + this.numerator + "/" + this.denominator + ")"
+  }
+}
